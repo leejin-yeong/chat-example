87a4082e90db6dd29c37f76ea146bba34e35fe39
JinYoung Lee <cookie178@naver.com>
2020-09-29 14:14:19 +0900

    start socket part

--- index.ts
+++ index.ts
@@ -2,16 +2,23 @@
 
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http); //initialize
 
 /*app.get('/',(req, res) => {
     res.send('<h1>Hello world</h1>');//display on webpage
 });*/
 // '/': route handler. website home.
 
+app.get('/',(req, res) => {
+    res.sendFile(__dirname+'/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    //새로고침 시 terminal에 뜸
+});
+
 http.listen(3000,()=>{
     console.log('listening on *:3000');
 });
 
-app.get('/',(req, res) => {
-    res.sendFile(__dirname+'/index.html');
-});
